7999c12288dac00d07cdccf273fb17ef501f3749
Christof Becu <christof.becu@gmail.com>
2020-01-27 11:08:05 +0100

    Scroll to top of page after filtering by tag

--- assets/js/script.js
+++ assets/js/script.js
@@ -59,6 +59,15 @@ function CreateCardDecks(repos) {
         htmlContent += `</div>`
     }
     divInner.innerHTML = htmlContent;
+    ScrollToTop();
+}
+
+function ScrollToTop() {
+    window.scrollTo({
+        top: 0,
+        left: 0
+        // behavior: 'smooth'
+      });
 }
 
 
